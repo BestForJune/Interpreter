1d453760c632700ff96f56e33751cbd393c9c761
ZhengsenF <42655384+ZhengsenF@users.noreply.github.com>
2019-10-11 13:56:29 -0400

    constructed

---
+++ interpreter.cpp
@@ -0,0 +1,16 @@
+#include "interpreter.h"
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    ifstream file;
+    file.open(argv[1], ios::in);
+
+    return EXIT_SUCCESS;
+}
+
+
+
